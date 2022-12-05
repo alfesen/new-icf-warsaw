5bfdf27d863346e74e43ed05e006b6cdb1ed0543
alfesen <89510380+alfesen@users.noreply.github.com>
2022-12-05 11:12:43 +0100

    Basic nav structure

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,17 @@
+import Navigation from '../Navigation/Navigation'
 import s from './Header.module.scss'
 
 const Header = () => {
   return (
     <div
-      className={s.header}
       style={{
         background:
-          'linear-gradient(#00000044, #00000044), url(https://pixabay.com/get/g10086a53aaa78b34f19f1aa4d3541098ad22871f781ad792cfd94c66d9e5c44e7514f398a3e3ec37fd9bd9ce710f9fb9_640.jpg) ',
-      }}>
-      <nav></nav>
+          'linear-gradient(#00000044, #00000044), url(https://pixabay.com/get/g10086a53aaa78b34f19f1aa4d3541098ad22871f781ad792cfd94c66d9e5c44e7514f398a3e3ec37fd9bd9ce710f9fb9_640.jpg) no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+      className={s.header}>
+      <Navigation />
       <h1 className={s.header__heading}>
         <span className={s['header__heading--primary']}>
           WELCOME TO ICF WARSAW
