ab18b9983d8de5f725ba4c096f45c66fe39980ea
alfesen <89510380+alfesen@users.noreply.github.com>
2022-12-09 11:27:19 +0100

    Add Announcements mobile markup

---
+++ src/models/UI/Card/Card.tsx
@@ -0,0 +1,14 @@
+import s from './Card.module.scss'
+
+type Props = {
+  className: string
+  children: React.ReactNode
+}
+
+const Card = (props: Props) => {
+  const {className, children} = props
+  const {card} = s
+  return <div className={`${className} ${card}`}>{children}</div>
+}
+
+export default Card
