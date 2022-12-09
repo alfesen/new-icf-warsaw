86b3a87b3642f8e9e43be7089f685df1bbfcc47d
alfesen <89510380+alfesen@users.noreply.github.com>
2022-12-09 10:20:56 +0100

    Restore lossess

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import Header from './components/Header/Header'
+import Home from './pages/Home'
+
 function App() {
   return (
     <div>
       <Header />
+      <main>
+        <Home />
+      </main>
     </div>
   )
 }
---
+++ src/views/Welcome/Welcome.tsx
@@ -0,0 +1,25 @@
+import s from './Welcome.module.scss'
+
+const Welcome = () => {
+  return (
+    <section className={`container ${s.welcome}`}>
+      <h2 className={s.welcome__heading}>Welcome to ICF Warsaw</h2>
+      <p className={s.welcome__text}>
+        The International Christian Fellowship of Warsaw is an English-speaking
+        church that worships Jesus Christ as Lord and Savior and the hope for
+        all people. We are a church family made up of people from over{' '}
+        <span>30 nations, dozens of religious backgrounds</span>, and many{' '}
+        <span>diverse cultures</span>. In our service to Jesus Christ we are
+        committed to providing{' '}
+        <span>
+          inspiring worship, relevant teaching, and authentic friendships
+        </span>
+        .  If you are looking for a church home while in Warsaw, or are just
+        curious as to who we are, we invite you to come and join us.  We would
+        love to meet you!
+      </p>
+    </section>
+  )
+}
+
+export default Welcome
