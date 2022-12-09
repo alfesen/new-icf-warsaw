65d4903c3f54ae43a87a1da213c39d9c7d8f593f
alfesen <89510380+alfesen@users.noreply.github.com>
2022-12-09 09:38:57 +0100

    Fix nav item colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import Header from './components/Header/Header'
+import Home from './pages/Home/Home'
 function App() {
   return (
     <div>
       <Header />
+      <main>
+        <Home />
+      </main>
     </div>
   )
 }
---
+++ src/views/Welcome/Welcome.tsx
@@ -0,0 +1,26 @@
+const Welcome = () => {
+  return (
+    <section className={`container`}>
+      <h2>Welcome to ICF Warsaw</h2>
+      <p>
+        The International Christian Fellowship of Warsaw is an English-speaking
+        church that worships Jesus Christ as Lord and Savior and the hope for
+        all people. We are a church family made up of people from over{' '}
+        <span>30 nations, dozens of religious backgrounds</span>, and many{' '}
+        <span>diverse cultures</span>. In our service to Jesus Christ we are
+        committed to providing{' '}
+        <span>
+          inspiring worship, relevant teaching, and authentic friendships
+        </span>
+        .  If you are looking for a church home while in Warsaw, or are just
+        curious as to who we are, we invite you to come and join us.  We would
+        love to meet you!
+      </p>
+      <div>
+        <h3></h3>
+      </div>
+    </section>
+  )
+}
+
+export default Welcome
