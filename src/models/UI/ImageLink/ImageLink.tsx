b222b8621e698e16f96ea0e5f50bdac6bdfd9c5c
alfesen <89510380+alfesen@users.noreply.github.com>
2022-12-09 13:18:49 +0100

    Create ImageLink component

---
+++ src/models/UI/ImageLink/ImageLink.tsx
@@ -0,0 +1,24 @@
+import s from './ImageLink.module.scss'
+import { Link } from 'react-router-dom'
+
+type Props = {
+  className?: string
+  image: string
+  text: string
+}
+
+const ImageLink = (props: Props) => {
+  const { className, image, text } = props
+  return (
+    <Link to='#' className={`${s.link} ${className}`}>
+      <div className={s.link__img}>
+        <img src={image} alt='' />
+        <div className={s.link__overlay}>
+          <h3 className={s.link__text}>{text}</h3>
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+export default ImageLink
