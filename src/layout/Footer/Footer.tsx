64f7b451ddedcee1492c34b86108e0eb973cf5ad
alfesen <89510380+alfesen@users.noreply.github.com>
2022-12-15 10:13:16 +0100

    Adjust mixins and complete mobile footer

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -6,6 +6,7 @@ import {
   faInstagramSquare,
 } from '@fortawesome/free-brands-svg-icons'
 import { Fade } from 'react-awesome-reveal'
+import Logo from '../../UI/Logo/Logo'
 
 const Footer = () => {
   const iconLinks = [
@@ -25,7 +26,23 @@ const Footer = () => {
       <div className='container'>
         <Fade>
           <div className={s.footer__social}>{renderIcons}</div>
+          <a href='' className={s.footer__rodo}>
+            ICFWarsaw Privacy Policy & RODO
+          </a>
         </Fade>
+        <hr />
+        <div className={s.footer__essentials}>
+          <p className={s['footer__essentials--address']}>
+            International Christian Fellowship, 326 Puławska, Warszawa,
+            mazowieckie, 02-845, Poland
+          </p>
+          <div className={s['footer__essentials--join']}>
+            <h2 className={s['footer__essentials--join-time']}>
+              Join us Sundays at 10:00am
+            </h2>
+            <Logo className={s.footer__logo} />
+          </div>
+        </div>
       </div>
     </footer>
   )
