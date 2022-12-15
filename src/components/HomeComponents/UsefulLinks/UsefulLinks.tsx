b63723d877c7972c92d12eb7e594246dbf5a95be
alfesen <89510380+alfesen@users.noreply.github.com>
2022-12-15 11:48:50 +0100

    Add directions link in Useful Links

--- src/components/HomeComponents/UsefulLinks/UsefulLinks.tsx
+++ src/components/HomeComponents/UsefulLinks/UsefulLinks.tsx
@@ -1,12 +1,15 @@
 import s from './UsefulLinks.module.scss'
 import donate from '../../../assets/images/links/donate-sm.jpg'
+import map from '../../../assets/images/links/map-sm.jpg'
 
 const UsefulLinks = () => {
   return (
     <div className={s.links}>
       <div className={s.link}>
-        <div className={s.link__text}>Where we are</div>
-        
+        <a href='/directions'>
+          <div className={s.link__text}>Where we are</div>
+          <img className={s.link__img} src={map} alt='Giving to ICF images' />
+        </a>
       </div>
       <div className={s.link}>
         <div className={s.link__text}>Donate</div>
