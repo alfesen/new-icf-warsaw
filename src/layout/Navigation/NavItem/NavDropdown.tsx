9794d343a4f092bddc97dba05262cc435995a94d
alfesen <89510380+alfesen@users.noreply.github.com>
2022-12-15 13:54:21 +0100

    Optimize NavDropdown code and fix broken links

--- src/layout/Navigation/NavItem/NavDropdown.tsx
+++ src/layout/Navigation/NavItem/NavDropdown.tsx
@@ -1,22 +1,27 @@
 import s from './NavDropdown.module.scss'
 
 const NavDropdown = ({ links }: { links: string[] }) => {
-  return (
-    <div className={s.links}>
-      {links &&
-        links.map(link => {
-          const linkQuery = link.replaceAll(' ', '').toLowerCase()
-          return (
-            <a
-              className={s.link}
-              key={`${link.replaceAll(' ', '')}_link`}
-              href={`/${linkQuery}`}>
-              {link}
-            </a>
-          )
-        })}
-    </div>
-  )
+  const renderLinks =
+    links &&
+    links.map(link => {
+      const linkQuery = link
+        .trim()
+        .replaceAll('/', '')
+        .replaceAll('%', 'per')
+        .replaceAll('& ', '')
+        .replaceAll(' ', '-')
+        .toLowerCase()
+      return (
+        <a
+          className={s.link}
+          key={`${link.replaceAll(' ', '')}_link`}
+          href={`/${linkQuery}`}>
+          {link}
+        </a>
+      )
+    })
+
+  return <div className={s.links}>{renderLinks}</div>
 }
 
 export default NavDropdown
